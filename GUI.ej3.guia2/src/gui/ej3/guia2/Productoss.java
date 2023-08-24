971e253ffdecad10d724772c0cdf8e324af66e98
Lourrios <141644661+Lourrios@users.noreply.github.com>
2023-08-24 11:08:58 -0300

    Add files via upload

---
+++ GUI.ej3.guia2/src/gui/ej3/guia2/Productoss.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.ej3.guia2;
+
+/**
+ *
+ * @author Lourdes
+ */
+public class Productoss {
+    
+    private String nombre;
+    private String categoria;
+    private  double precio;
+
+    public Productoss(String nombre, String categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + categoria   + precio;
+    }
+    
+    
+    
+    
+}
